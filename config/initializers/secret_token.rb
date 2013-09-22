db2d9b70687e35f6bddcd208983e4b2a06f80bbb
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-09-22 18:16:05 -0400

    First Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HartlOne::Application.config.secret_key_base = 'defd224745ad8051d8f58c9e9e11dd967092aee67475955acc47045690e6a3d9b40a6b8c4a9089b355914cd8334658fabf181a892c9a612ce5059d7e6059a0b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HartlOne::Application.config.session_store :cookie_store, key: '_hartl_one_session'
